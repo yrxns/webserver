c03e143477a71e88eaa9f9e9687f84b244c0a7b2
yrxns <819924033@qq.com>
2022-11-16 18:40:01 +0800

    ...

--- test/client.cpp
+++ test/client.cpp
@@ -2,7 +2,9 @@
 #include <unistd.h>
 
 int main() {
-
+    tcpsock client;
+    client.connect("127.0.0.1", 8890);
+    write(client, "client...", 10);
 
     return 0;
 }
--- test/server.cpp
+++ test/server.cpp
@@ -1,5 +1,6 @@
 #include "../tcpsock/tcpsock.h"
 #include <unistd.h>
+#include <arpa/inet.h>
 #include <iostream>
 
 int main() {
@@ -7,13 +8,18 @@ int main() {
     server.bind("127.0.0.1", 8890);
     server.listen();
 
-    int clnt_sock = server.accept(nullptr);
-    /* struct sockaddr_in clnt_addr; */
-    /* int clnt_sock = server.accept(&clnt_addr); */
+    /* int clnt_sock = server.accept(nullptr); */
+    struct sockaddr_in clnt_addr;
+    int clnt_sock = server.accept(&clnt_addr);
 
     char buf[24];
     read(clnt_sock, buf, 23);
     std::cout << buf << std::endl;
+    std::cout << "client addr: " << clnt_addr.sin_addr.s_addr << std::endl;
+    std::cout << "client port: " << clnt_addr.sin_port << std::endl;
+    char s[120];
+    inet_ntop(AF_INET, &clnt_addr.sin_addr.s_addr, s, 120);
+    std::cout << "client addr: " << s << std::endl;
 
     close(clnt_sock);
     close(server);
